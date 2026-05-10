779b83bfaf6ddddb6481a9d378b5f1ffd4c5846f
MrDrist <abobachebaksar@gmail.com>
2026-05-10 18:55:23 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,6 +10,7 @@ export const COUNTRIES = {
     "portugal": { name: "Португалия", color: "#105d10", leader: "Антониу Салазар", ideology: "Нейтралитет" },
     "netherlands": { name: "Нидерланды", color: "#f97316", leader: "Вильгельмина", ideology: "Демократия" },
     "belgium": { name: "Бельгия", color: "#eab308", leader: "Леопольд III", ideology: "Демократия" },
+    "luxembourg": { name: "Люксембург", color: "#67e8f9", leader: "Шарлотта", ideology: "Демократия" },
     "switzerland": { name: "Швейцария", color: "#dc2626", leader: "Джузеппе Мотта", ideology: "Демократия" },
     "romania": { name: "Румыния", color: "#eab308", leader: "Кароль II", ideology: "Нейтралитет" },
     "hungary": { name: "Венгрия", color: "#166534", leader: "Миклош Хорти", ideology: "Нейтралитет" },
@@ -19,7 +20,11 @@ export const COUNTRIES = {
     "austria": { name: "Австрия", color: "#ef4444", leader: "Курт Шушниг", ideology: "Нейтралитет" },
     "denmark": { name: "Дания", color: "#ef4444", leader: "Кристиан X", ideology: "Демократия" },
     "greece": { name: "Греция", color: "#60a5fa", leader: "Иоаннис Метаксас", ideology: "Нейтралитет" },
-    "yugoslavia": { name: "Югославия", color: "#1e3a8a", leader: "Пётр II", ideology: "Нейтралитет" }
+    "yugoslavia": { name: "Югославия", color: "#1e3a8a", leader: "Пётр II", ideology: "Нейтралитет" },
+    "lithuania": { name: "Литва", color: "#065f46", leader: "Антанас Сметона", ideology: "Нейтралитет" },
+    "latvia": { name: "Латвия", color: "#8b0000", leader: "Карлис Улманис", ideology: "Нейтралитет" },
+    "estonia": { name: "Эстония", color: "#4682b4", leader: "Константин Пятс", ideology: "Нейтралитет" },
+    "slovakia": { name: "Словакия", color: "#60a5fa", leader: "Йозеф Тисо", ideology: "Фашизм" }
 };
 
 // ХАРАКТЕРИСТИКИ ЮНИТОВ
